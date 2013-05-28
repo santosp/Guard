679c430db6e60fb000e0aab5b21c8148edcc0acd
santosp <santospaul253@gmail.com>
2013-05-27 20:44:57 -0700

    UI Overhaul working with add and remove, they correctly search for valid UPC and return weather it is found or not

--- main.c
+++ main.c
@@ -26,7 +26,7 @@
 #include "Items.h"
 /*=======*/
 //Globals
-UISTATEINFO StateInfo={REMOVE,GETUPC}; // NEED TO START IN WELCOME not MAIN
+UISTATEINFO StateInfo={REMOVE,INIT}; // NEED TO START IN WELCOME not MAIN
 UISTATE LastState=MAIN;
 
 
@@ -60,6 +60,7 @@ Void UiTask(UArg a0, UArg a1)
 	INT8U key=0;
 	INT8U upcbuffer[12]={0,0,0,0,0,0,0,0,0,0,0,0};
 	INT8U upccount=0,inputcount=0,itemfound=0,lookupcount=0,upc_count=0;
+	INT8U clearcount=0;
 	while(1){
 		//poll buttons
 		//Task_delete(testTask);
@@ -134,6 +135,14 @@ Void UiTask(UArg a0, UArg a1)
 				switch (StateInfo.StateStatus){
 				case INIT:
 					//Initialize state
+					draw_text_bmp((INT8U *)"ADD STATE ",16,40,MyFont,1);
+					StateInfo.StateStatus = GETUPC;
+					draw_text_bmp((INT8U *)"         ",16,24,MyFont,1);// clear found or not space
+					draw_text_bmp((INT8U *)"             ",16,48,MyFont,1);//clear upcbuffer space
+					for(clearcount=0;clearcount<11;clearcount++){// clear buffer
+						upcbuffer[clearcount]=0;
+					}
+					upccount=0;
 				break;//End of INIT State
 				/*-----------------------------------------------------------*/
 				case GETUPC:
@@ -146,7 +155,7 @@ Void UiTask(UArg a0, UArg a1)
 					key=0;
 					if(upccount>10){
 						upccount=0;
-						for(inputcount=0;(upcbuffer[inputcount]<='9'&&upcbuffer[inputcount]>='0')||inputcount>0;inputcount++);
+						for(inputcount=0;(upcbuffer[inputcount]<='9'&&upcbuffer[inputcount]>='0')&&inputcount<11;inputcount++);
 						if(inputcount>10){
 							//valid input
 							StateInfo.StateStatus=FIND;
@@ -154,33 +163,36 @@ Void UiTask(UArg a0, UArg a1)
 						else{
 							//invalid input, i.e. non number characters
 							StateInfo.StateStatus=INVALID;
+							draw_text_bmp((INT8U *)"INVALID ",16,24,MyFont,1);
 						}
 					}
 				break;//End of GETUPC State
 				/*-----------------------------------------------------------*/
 				case INVALID:
-					////invalid user input, i.e. non number characters,
-				break;//End of INVALID state
+				break;//End of INVALID State
 				/*-----------------------------------------------------------*/
 				case FIND://Looks for a UPC match
-					for(lookupcount=0;(lookupcount<25||itemfound==1);lookupcount++){
-						if(ItemLookUp[lookupcount][1]>0){
+					for(lookupcount=0;(lookupcount<25&&itemfound!=1);lookupcount++){
+						if(ItemLookUp[lookupcount][0]>0){
 							for(upc_count=0;upcbuffer[upc_count]==ItemBlock1[lookupcount].UPC[upc_count]&&upc_count<11;upc_count++);
-							if(lookupcount<11){
+							if(upc_count<11){
 								//No match
 								//Give user option to either re enter or add to inventory
 								StateInfo.StateStatus = NOMATCH;
+								//draw_text_bmp((INT8U *)"NO Match ",16,24,MyFont,1);
 							}
 							else{
 								//found item
 								itemfound=1;
 								StateInfo.StateStatus = FOUND;
+								draw_text_bmp((INT8U *)"FOUND ",16,24,MyFont,1);
 							}
 						}
 						else{
 							//No item of this UPC number
 							//Give user option to either re enter or add to inventory
 							StateInfo.StateStatus = NOMATCH;
+							draw_text_bmp((INT8U *)"NO Match ",16,24,MyFont,1);
 						}
 					}
 				break;//End of Find
@@ -200,6 +212,14 @@ Void UiTask(UArg a0, UArg a1)
 				switch (StateInfo.StateStatus){
 				case INIT:
 					//Initialize state
+					draw_text_bmp((INT8U *)"REMOVE STATE ",16,40,MyFont,1);
+					StateInfo.StateStatus = GETUPC;
+					draw_text_bmp((INT8U *)"         ",16,24,MyFont,1);// clear found or not space
+					draw_text_bmp((INT8U *)"             ",16,48,MyFont,1);//clear upcbuffer space
+					for(clearcount=0;clearcount<11;clearcount++){// clear buffer
+						upcbuffer[clearcount]=0;
+					}
+					upccount=0;
 				break;//End of INIT State
 				/*-----------------------------------------------------------*/
 				case GETUPC:
@@ -212,7 +232,7 @@ Void UiTask(UArg a0, UArg a1)
 					key=0;
 					if(upccount>10){
 						upccount=0;
-						for(inputcount=0;(upcbuffer[inputcount]<='9'&&upcbuffer[inputcount]>='0')||inputcount>0;inputcount++);
+						for(inputcount=0;(upcbuffer[inputcount]<='9'&&upcbuffer[inputcount]>='0')&&inputcount<11;inputcount++);
 						if(inputcount>10){
 							//valid input
 							StateInfo.StateStatus=FIND;
@@ -220,6 +240,7 @@ Void UiTask(UArg a0, UArg a1)
 						else{
 							//invalid input, i.e. non number characters
 							StateInfo.StateStatus=INVALID;
+							draw_text_bmp((INT8U *)"INVALID ",16,24,MyFont,1);
 						}
 					}
 				break;//End of GETUPC State
@@ -228,24 +249,27 @@ Void UiTask(UArg a0, UArg a1)
 				break;//End of INVALID State
 				/*-----------------------------------------------------------*/
 				case FIND://Looks for a UPC match
-					for(lookupcount=0;(lookupcount<25||itemfound==1);lookupcount++){
-						if(ItemLookUp[lookupcount][1]>0){
+					for(lookupcount=0;(lookupcount<25&&itemfound!=1);lookupcount++){
+						if(ItemLookUp[lookupcount][0]>0){
 							for(upc_count=0;upcbuffer[upc_count]==ItemBlock1[lookupcount].UPC[upc_count]&&upc_count<11;upc_count++);
-							if(lookupcount<11){
+							if(upc_count<11){
 								//No match
 								//Give user option to either re enter or add to inventory
 								StateInfo.StateStatus = NOMATCH;
+								//draw_text_bmp((INT8U *)"NO Match ",16,24,MyFont,1);
 							}
 							else{
 								//found item
 								itemfound=1;
 								StateInfo.StateStatus = FOUND;
+								draw_text_bmp((INT8U *)"FOUND ",16,24,MyFont,1);
 							}
 						}
 						else{
 							//No item of this UPC number
 							//Give user option to either re enter or add to inventory
 							StateInfo.StateStatus = NOMATCH;
+							draw_text_bmp((INT8U *)"NO Match ",16,24,MyFont,1);
 						}
 					}
 				break;//End of FIND State
@@ -259,20 +283,7 @@ Void UiTask(UArg a0, UArg a1)
 				default:
 				break;//End of Default
 				}//End of StateStatus REMOVE switch
-#if 0
-				else{key=KeyPend(10);
-					if(key>0&&key<0x7E){
-						upcbuffer[upccount]=key;
-						upccount++;
-						draw_text_bmp(upcbuffer,16,48,MyFont,1);
-					}
-					if(upccount>10){
-						upccount=0;
-					}
-					key=0;
-					draw_text_bmp((INT8U *)"REMOVE STATE ",16,40,MyFont,1);
-				}
-#endif
+
 			break;//End of Remove State
 			/*===============================================================*/
 			case INV:
